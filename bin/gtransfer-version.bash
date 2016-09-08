d1a82c1d89bb766bc4a0a3ce345776bcfd3ba21f
Frank Scheiner <scheiner@hlrs.de>
2016-09-08 09:52:50 +0200

    Updated spec file, gtransfer-version and change log accordingly.

--- bin/gtransfer-version.bash
+++ bin/gtransfer-version.bash
@@ -29,7 +29,7 @@ COPYRIGHT
 ################################################################################
 
 readonly _program=$( basename "$0" )
-readonly _gtransferToolkitVersion="0.7.0"
+readonly _gtransferToolkitVersion="0.7.1"
 
 readonly _gtransferTools=( "gtransfer"
 			   "dpath"
